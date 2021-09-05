29acc528a346339b38c687088424f2da7ada8fb9
Sergey Antonov <superflasher22@gmail.com>
2021-09-05 22:47:54 +0300

    Configure terser

--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -2,11 +2,37 @@ import { terser } from 'rollup-plugin-terser';
 import filesize from 'rollup-plugin-filesize';
 import base from './rollup.config';
 
+const properties = [
+	'getColorTransform',
+	'getMatrix',
+	'concat',
+	'empty',
+	'onUpdate',
+	'scale',
+	'scaleX',
+	'scaleY',
+	'rotation',
+	'pallete',
+	'children',
+	'shape',
+	'alpha',
+	'color',
+	// 'value',
+	'tint',
+	'brightness',
+	'render',
+	'update',
+	'getWidth',
+	'getHeight',
+];
+
+const regex = new RegExp(`${properties.join('|')}`);
+
 export default {
 	...base,
 	plugins: [
 		...base.plugins,
-		terser(),
+		terser({ mangle: { properties: { builtins: true, regex } } }),
 		filesize(),
 	],
 };
