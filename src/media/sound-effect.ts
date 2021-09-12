1c3efcc16d0a88b7066d6a34f27ed070060ed506
Sergey Antonov <superflasher22@gmail.com>
2021-09-12 09:26:58 +0300

    Correct sounds

--- src/media/sound-effect.ts
+++ src/media/sound-effect.ts
@@ -3,25 +3,13 @@ declare function jsfxr(params: number[]): string;
 const LASER = jsfxr([0, 0, 0.2996286612451152, 0.049831483062296344, 0.15737676185023775, 0.5703562729712254, 0.037420144186540515, -0.3879256862405088, 0, 0, 0, 0, 0, 0.18821061188407817, 0.09033692527451312, 0, 0, 0, 1, 0, 0, 0.024003649437587614, 0, 0.5]);
 const EXPLOSION = jsfxr([3, 0, 0.3208591283506936, 0.5818983553245236, 0.4412527797139363, 0.08430907838621725, 0, -0.3216593280968755, 0, 0, 0, 0, 0, 0, 0, 0, 0.1444287878082801, -0.11652028380910384, 1, 0, 0, 0, 0, 0.5]);
 
-let laserPlayed = false;
-
 export function playLaser(volume: number) {
-	if (laserPlayed) {
-		console.log('skip')
-		return;
-	}
-
 	const laserSound = new Audio();
 	laserSound.volume = volume;
 	laserSound.src = LASER;
 	laserSound.play().catch(() => {
 		// console.log(e);
 	});
-
-	// laserPlayed = true;
-	// setTimeout(() => {
-	// 	laserPlayed = false;
-	// }, 100);
 }
 
 export function playExplosion(volume: number) {
